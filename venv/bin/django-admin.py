f3c86250c143cdcdb145df22c166f91f78b8803b
eMIKARp <emil.karpowicz@gmail.com>
2019-10-04 16:15:34 +0200

    Initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/emikarp/Dokumenty/01_Moje_programy/MyDjango/Place_Order/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
