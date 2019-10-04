a5b971303789fedb76a241301a9df451b9aa8210
eMIKARp <emil.karpowicz@gmail.com>
2019-10-04 22:11:47 +0200

    Controllers added

--- Place_Order/urls.py
+++ Place_Order/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path,include
+from Place_Order_Application import views
 
 urlpatterns = [
+    re_path(r'^$',views.index,name='index'),
+    re_path(r'^',include('Place_Order_Application/urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ Place_Order_Application/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,re_path,include
+from Place_Order_Application import views
+
+urlpatterns = [
+    re_path(r'^index',views.index,name="index2"),
+    re_path(r'^place_order',views.place_order,name="place_order"),
+    re_path(r'^guest',views.guest,name="guest"),
+    re_path(r'^check_products',views.check_products,name="check_products"),
+    re_path(r'^check_orders',views.check_orders,name="check_orders"),
+    re_path(r'^administrator',views.administrator,name="administrator"),
+    re_path(r'^add_user',views.add_user,name="add_user"),
+    re_path(r'^add_product',views.add_product,name="add_product"),
+
+]
